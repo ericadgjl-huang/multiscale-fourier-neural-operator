7a2e1d1c89bb4063d3f17bbb013ef4955dc36c69
ericadgjl-huang <ericadgjl@gmail.com>
2026-06-26 21:09:46 +0800

    run_A.bat 補回漏掉的 unet_2d，A 機回到 4 個架構

--- run_A.bat
+++ run_A.bat
@@ -1,6 +1,9 @@
 @echo off
 set KMP_DUPLICATE_LIB_OK=TRUE
 
+python fourier_2d.py unet_2d --data-glob "data/global_era5_96_factors_*.nc" --output-root outputs96
+if errorlevel 1 exit /b 1
+
 python fourier_2d.py sufno --data-glob "data/global_era5_96_factors_*.nc" --output-root outputs96
 if errorlevel 1 exit /b 1
 
